61c424898080a762b8a8989e6e0729ba00558b3a
Orlandovpjunior <orlando.virginio.penha.junior@ccc.ufcg.edu.br>
2025-05-21 16:08:18 -0300

    chore: adiciona utils

--- backend/utils.py
+++ backend/utils.py
@@ -3,7 +3,6 @@ from pprint import pprint
 from elasticsearch import Elasticsearch
 
 def get_es_client(max_retries: int = 5, sleep_time: int = 5) -> Elasticsearch:
-
     i = 0
     while i < max_retries:
         try:
@@ -14,5 +13,6 @@ def get_es_client(max_retries: int = 5, sleep_time: int = 5) -> Elasticsearch:
             return es
         except Exception:
             pprint('Could not connect to Elasticsearch, retrying...')
+            time.sleep(sleep_time)
             i += 1
     raise ConnectionError("Failed to connect to Elasticsearch after multiple attempts.")
